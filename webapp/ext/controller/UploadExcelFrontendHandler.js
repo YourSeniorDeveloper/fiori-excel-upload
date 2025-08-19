8df4e3c0efa402d15ca045bfac94a0bf6c968e84
Carlos Ribeiro <carloscde@gmail.com>
2025-08-19 12:46:07 +0000

    update action button

--- webapp/ext/controller/UploadExcelFrontendHandler.js
+++ webapp/ext/controller/UploadExcelFrontendHandler.js
@@ -5,7 +5,139 @@ sap.ui.define([
 
     return {
         UploadExcelFrontend: function(oEvent) {
-            MessageToast.show("Custom handler invoked.");
+            //MessageToast.show("Custom handler invoked.");
+            this._callUploadDialog("/ImportarPlanilha", "Lista de Materiais");
+        },
+        _callUploadDialog: function(action, orig) {
+            if(!this.oFileUploadDialog) {
+                this._createFileUploadDialog(orig, function (oFile, sFilename) {
+                        this._callAction(
+                            action,
+                            {
+                                "filename": sFilename,
+                                "arquivo_base64": btoa(oFile.target.result)
+                            },
+                            function (data) {
+                                this.templateBaseExtension.getExtensionAPI().refreshTable();
+                            }.bind(this)
+                        );
+                    }.bind(this)
+                );
+            }
+            this.oFileUploadDialog.open();
+        },
+        _createFileUploadDialog: function(orig, onFileUploadHandler) {
+            this.oFileUploader = new sap.ui.unified.FileUploader("FileUploader",{
+                name: "FileUploader",
+                tooltip: "Importar arquivo",
+                fileType: ["xlsx","XLSX"],
+                width:"350px"
+            });
+
+            this.oFileUploadDialog = new sap.m.Dialog({
+                title: "Realizar carga de " + orig,
+                content: [ 
+                    new sap.ui.layout.form.SimpleForm("FileUploadForm", {
+                        editable: true,
+                        content: [
+                            this.oFileUploader
+                        ]
+                    })
+                ],
+                beginButton: new sap.m.Button({
+                    type: sap.m.ButtonType.Emphasized,
+                    text: "Importar",
+                    press: function() {
+                        const file = this.oFileUploader.oFileUpload.files[0];
+                        this.filename = file.name;
+                        if (!file) {
+                            sap.m.MessageBox.show(
+                                "Nenhum arquivo foi selecionado",
+                                {
+                                    icon: sap.m.MessageBox.Icon.ERROR,
+                                    title: "Erro ao importar o arquivo"
+                                }
+                            );
+                            return;
+                        }
+                        
+                        const fileReader = new FileReader();
+                        
+                        fileReader.onload = function (oFile) {
+                            this.oFileUploadDialog.close();
+                            onFileUploadHandler(oFile, this.filename);
+                        }.bind(this);
+                        
+                        fileReader.onerror = function (oError) {
+                            sap.m.MessageBox.show(
+                                oError.message,
+                                {
+                                    icon: sap.m.MessageBox.Icon.ERROR,
+                                    title: "Erro ao importar o arquivo"
+                                }
+                            );
+                        };
+                        
+                        fileReader.readAsBinaryString(file);
+                    }.bind(this)
+                }),
+                endButton: new sap.m.Button({
+                    text: sap.ui.getCore().getLibraryResourceBundle("sap.ui.generic.app").getText("ACTION_CANCEL"),
+                    press: function () {
+                        this.oFileUploadDialog.close();
+                    }.bind(this)
+                }),
+                afterClose: function (oControlEvent) {
+                    this.oFileUploadDialog.destroy();
+                    delete this.oFileUploadDialog;
+                }.bind(this)
+            });
+        },
+        _callAction: function(action, oParameters, fnReturn) {
+            this.extensionAPI.securedExecution(
+                function(){
+                    return new Promise(function (resolve, reject) {
+                        this.extensionAPI.invokeActions(
+                            action,
+                            [this.getView().getBindingContext()],
+                            oParameters,
+                            { 
+                                bInvocationGroupingChangeSet: false 
+                            }
+                        ).then(
+                            function (data) {
+                                if (fnReturn) {
+                                    fnReturn(data);
+                                }
+                                resolve(data);
+                            },
+                            function (error) {
+                                reject(error);
+                            }
+                        ).catch(function (error) {
+                            reject(error);
+                        });
+                    }.bind(this));            
+                }.bind(this), 
+                { 
+                    busy: {
+                        set: true,
+                        check: true
+                    },
+                    dataloss: {
+                        popup: true,
+                        navigation: false
+                    }
+                }
+            ).then(
+                function (data) {
+                    return;
+                },
+                function (error) {
+                    sap.m.MessageToast.show("Erro ao realizar a chamada do serviço " + action);
+                    console.log(error);
+                }
+            );
         }
     };
 });
